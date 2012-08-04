5c21c121c23d6cffc6388337522376c916490e24
void <void256@users.sourceforge.net>
2012-08-04 21:47:39 +0200

    add ortho projection to MatrixFactory and made methods static

--- src/main/java/de/lessvoid/coregl/MatrixFactory.java
+++ src/main/java/de/lessvoid/coregl/MatrixFactory.java
@@ -4,7 +4,7 @@ package de.lessvoid.coregl;
 import org.lwjgl.util.vector.Matrix4f;
 
 public class MatrixFactory {
-  public Matrix4f createProjection(final float width, final float height) {
+  public static Matrix4f createOrtho(final float width, final float height) {
     float left = 0;
     float right = width;
     float top = 0;
@@ -22,4 +22,24 @@ public class MatrixFactory {
     projection.m33 = 1;
     return projection;
   }
+
+  public static Matrix4f createProjection(final float width, final float height) {
+    float left = 0;
+    float right = width;
+    float top = 0;
+    float bottom = height;
+    float zNear = 1;
+    float zFar = 10000;
+
+    Matrix4f projection = new Matrix4f();
+    projection.m00 = 2 * zNear / (right-left);
+    projection.m20 = (right+left) / (right-left);
+    projection.m11 = 2 * zNear / (top-bottom);
+    projection.m21 = (top+bottom) / (top-bottom);
+    projection.m22 = -(zFar + zNear) / (zFar-zNear);
+    projection.m32 = - 2 * zFar * zNear / (zFar-zNear);
+    projection.m23 = -1;
+    return projection;
+  }
+
 }
