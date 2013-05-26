a35546a76d890dbe7461093b2ce74e71b25070ca
void <void256@users.sourceforge.net>
2013-05-26 02:01:20 +0200

    some tweaks to the Screenshot class

--- lwjgl-coregl-utils/src/main/java/de/lessvoid/Screenshot.java
+++ lwjgl-coregl-utils/src/main/java/de/lessvoid/Screenshot.java
@@ -2,7 +2,9 @@ package de.lessvoid;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL11.GL_STENCIL_INDEX;
 import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glPixelStorei;
 import static org.lwjgl.opengl.GL11.glReadPixels;
 
 import java.awt.image.BufferedImage;
@@ -18,28 +20,58 @@ import org.lwjgl.BufferUtils;
 import de.lessvoid.coregl.CoreCheckGL;
 
 public class Screenshot {
-  public void save(final String filename, int width, int height) throws IOException {
+  public void color(final String filename, int width, int height) throws IOException {
     BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
     ByteBuffer pixels = BufferUtils.createByteBuffer(width*height*3);
+    glPixelStorei(GL_PACK_ALIGNMENT, 1);
+    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    CoreCheckGL.checkGLError("glReadPixels");
+
+    WritableRaster raster = bi.getRaster();
+    int[] buffer = new int[width*height*3];
+    for (int i=0; i<width*height*3; i++) {
+      buffer[i] = pixels.get(i);
+    }
+    raster.setPixels(0, 0, width, height, buffer);
+
+    File outputfile = new File(filename);
+    ImageIO.write(bi, "png", outputfile);
+  }
+
+  public void stencil(final String filename, int width, int height) throws IOException {
+    BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+    ByteBuffer pixels = BufferUtils.createByteBuffer(width*height);
     glPixelStorei(GL_PACK_ALIGNMENT, 1);
     glReadPixels(0, 0, width, height, GL_STENCIL_INDEX, GL_UNSIGNED_BYTE, pixels);
     CoreCheckGL.checkGLError("glReadPixels");
 
     WritableRaster raster = bi.getRaster();
-    int[] pixel = new int[3];
+    int[] pixel = new int[1];
     for (int y=0; y<height; y++) {
       for (int x=0; x<width; x++) {
-        pixel[0] = pixels.get(y*width*3+x*3+0);
-        pixel[1] = pixels.get(y*width*3+x*3+1);
-        pixel[2] = pixels.get(y*width*3+x*3+2);
+        pixel[0] = pixels.get(y*width+x)*255;
         System.out.print(pixel[0] + ", ");
-        System.out.print(pixel[1] + ", ");
-        System.out.print(pixel[2] + ", ");
         raster.setPixel(x, y, pixel);
       }
       System.out.println();
     }
     File outputfile = new File(filename);
     ImageIO.write(bi, "png", outputfile);
+  }
+
+  public static void saveStencil(final String filename, int width, int height) {
+    try {
+      new Screenshot().stencil(filename, width, height);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void saveColor(final String filename, int width, int height) {
+    try {
+      new Screenshot().color(filename, width, height);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
