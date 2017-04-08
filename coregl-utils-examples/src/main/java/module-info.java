c51b9b15209c9d1e2b5f53918e5a8b100aea7468
void256 <void@lessvoid.com>
2017-04-09 00:41:53 +0200

    chore: enable LWJGL3 in example module and explain how it works

--- coregl-utils-examples/src/main/java/module-info.java
+++ coregl-utils-examples/src/main/java/module-info.java
@@ -3,7 +3,19 @@
  */
 module com.lessvoid.coregl.examples {
   requires java.desktop;
-  //requires com.lessvoid.coregl.lwjglthree;
+
+  // currently the first service provider available is being used so to
+  // actual control which one is active we only enable a single one here.
+  //
+  // this is a little clumsy since this way we can't decide that at runtime
+  // but for demonstrating purpose it should be enough.
+
+  // enable LWJGL3
+  requires com.lessvoid.coregl.lwjglthree;
+
+  // enable LWJGL
   //requires com.lessvoid.coregl.lwjgl;
-  requires com.lessvoid.coregl.jogl;
+
+  // enable JOGL
+  //requires com.lessvoid.coregl.jogl;
 }
