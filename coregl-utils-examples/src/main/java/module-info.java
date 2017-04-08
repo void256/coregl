68f418f811ec1b4b0e543de04160631f5a61fa3e
void256 <void@lessvoid.com>
2017-04-09 00:54:17 +0200

    chore: fix comments in module-info.java
      // currently the first service provider available is being used so to
      // actual control which one is active we only enable a single one here.
      //
      // this is a little clumsy since this way we can't decide that at runtime
      // but for demonstrating purpose it should be enough.

--- coregl-utils-examples/src/main/java/module-info.java
+++ coregl-utils-examples/src/main/java/module-info.java
@@ -3,19 +3,7 @@
  */
 module com.lessvoid.coregl.examples {
   requires java.desktop;
-
-  // currently the first service provider available is being used so to
-  // actual control which one is active we only enable a single one here.
-  //
-  // this is a little clumsy since this way we can't decide that at runtime
-  // but for demonstrating purpose it should be enough.
-
-  // enable LWJGL3
   requires com.lessvoid.coregl.lwjglthree;
-
-  // enable LWJGL
   //requires com.lessvoid.coregl.lwjgl;
-
-  // enable JOGL
   //requires com.lessvoid.coregl.jogl;
 }
